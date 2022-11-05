915452b3cbff38c5462a6526f5dda16dbee72723
Esa Niemi <esa.niemi@protonmail.com>
2022-11-05 21:25:13 +0200

    Removed commented out code

--- src/analysis.js
+++ src/analysis.js
@@ -4,8 +4,6 @@ import intersect from "@turf/intersect";
 export const calculatePopulation = async (population, isochrone) => {
   const populationInRange = population.features
     .filter((popF) => {
-      // if (f.geometry.type === 'MultiPolygon')
-      // console.log(f.geometry.type, isochrone.type);
       let intersects = false;
       for (let i = 0; i < isochrone.features.length; i++) {
         const isoF = isochrone.features[i];
@@ -17,8 +15,6 @@ export const calculatePopulation = async (population, isochrone) => {
       return intersects;
     })
     .reduce((prev, current) => current.properties["ASUKKAITA"] + prev, 0);
-
-  console.log(populationInRange);
   return populationInRange;
 };
 
@@ -26,9 +22,3 @@ export const calculatePopulationScore = (population) => {
   if (population < 10000) return 0.01 * population - 1000;
   else return Math.min(0.009 * population - 900, 900);
 };
-
-// (async () => {
-//   //const a = { type: "FeatureCollection", features: await intersection() };
-//   console.log(await intersection());
-//   //await fs.writeFileSync("äitis", JSON.stringify(a));
-// })();
